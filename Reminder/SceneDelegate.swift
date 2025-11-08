795da13c5e6164421276fb40d9990a16396a3f88
Antonio Vuono <antoniosvuono@icloud.com>
2025-11-08 16:22:38 -0300

    feature: Splash Screen started

--- Reminder/SceneDelegate.swift
+++ Reminder/SceneDelegate.swift
@@ -17,11 +17,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         let window = UIWindow(windowScene: windowScene)
         
-        let viewController = UIViewController()
+//        let viewController = UIViewController()
+//        
+//        viewController.view.backgroundColor = .red
         
-        viewController.view.backgroundColor = .red
-        
-        let navigation = UINavigationController(rootViewController: viewController)
+        let navigation = UINavigationController(rootViewController: SplashViewController())
         
         window.rootViewController = navigation
         
