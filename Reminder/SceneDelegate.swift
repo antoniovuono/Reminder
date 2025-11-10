020f80fc867dd289adc2d19204b4c6253f97d43a
Antonio Vuono <antoniosvuono@icloud.com>
2025-11-10 18:02:06 -0300

    feature: Splash Screen finalized

--- Reminder/SceneDelegate.swift
+++ Reminder/SceneDelegate.swift
@@ -17,10 +17,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         let window = UIWindow(windowScene: windowScene)
         
-//        let viewController = UIViewController()
-//        
-//        viewController.view.backgroundColor = .red
-        
         let navigation = UINavigationController(rootViewController: SplashViewController())
         
         window.rootViewController = navigation
--- Reminder/Sources/Features/Splash/SplashViewController.swift
+++ Reminder/Sources/Features/Splash/SplashViewController.swift
@@ -13,13 +13,14 @@ class SplashViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         setup()
     }
     
     private func setup() {
         self.view.addSubview(contentView)
-        
+        self.view.backgroundColor = Colors.primaryRedBase
+        self.navigationController?.navigationBar.isHidden = true
+
         setupConstraints()
     }
     
