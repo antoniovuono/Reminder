e261c64241c4187f97450cd1eb5dcfb2306dcd81
Antonio Vuono <antoniosvuono@icloud.com>
2025-11-10 17:59:37 -0300

    feature: Design System constants and images inserted

---
+++ Reminder/Sources/Features/Constants/Metrics/Metrics-Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Metrics-Constants.swift
+//  Reminder
+//
+//  Created by Antonio Vuono on 10/11/25.
+//
+
+import Foundation
+import UIKit
+
+public struct Metrics {
+    static var huge: CGFloat = 48
+    static var medium: CGFloat = 24
+    static var small: CGFloat = 12
+    static var tiny: CGFloat = 8
+    static var little: CGFloat = 4
+}
